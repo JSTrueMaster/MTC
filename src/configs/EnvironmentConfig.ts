79b5ba1921ddcf95f0d3ae45d9650c2ae7816907
JSTrueMaster <richard.buttler.co@gmail.com>
2024-03-08 10:08:23 +0200

    changed the server URL

--- src/configs/EnvironmentConfig.ts
+++ src/configs/EnvironmentConfig.ts
@@ -1,25 +1,23 @@
 const dev = {
-	API_ENDPOINT_URL: 'http://localhost:3000',
-	IMAGE_PATH : 'http://localhost:3000/uploads/store/images/',
-	FILE_PATH : 'http://localhost:3000/uploads/store/docs/',
-	
+  API_ENDPOINT_URL: "http://172.252.85.10:3000",
+  IMAGE_PATH: "http://172.252.85.10:3000/uploads/store/images/",
+  FILE_PATH: "http://172.252.85.10:3000/uploads/store/docs/",
 };
 
 const prod = {
-	API_ENDPOINT_URL: 'http://localhost:3000',
-	IMAGE_PATH : 'http://localhost:3000/uploads/store/images/',
-	FILE_PATH : 'http://localhost:3000/uploads/store/docs/',
-	
+  API_ENDPOINT_URL: "http://172.252.85.10:3000",
+  IMAGE_PATH: "http://172.252.85.10:3000/uploads/store/images/",
+  FILE_PATH: "http://172.252.85.10:3000/uploads/store/docs/",
 };
 
 const getEnv = () => {
-	switch (process.env.NODE_ENV) {
-		case 'development':
-			return dev
-		case 'production':
-			return prod
-		default:
-			throw new Error('Invalid NODE_ENV value.');
-	}
+  switch (process.env.NODE_ENV) {
+    case "development":
+      return dev;
+    case "production":
+      return prod;
+    default:
+      throw new Error("Invalid NODE_ENV value.");
+  }
 };
-export const env = getEnv()
+export const env = getEnv();
